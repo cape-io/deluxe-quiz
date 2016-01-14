70db6ad331101133e5493653a179a0f8fb64eec0
Kai Curry <kai@cape.io>
2016-01-14 17:12:31 -0600

    populate init data

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import React from 'react'
 import { render } from 'react-dom'
 // Add the latest es2016+ stuff that we love.
 import 'babel-polyfill'
+import 'isomorphic-fetch'
 
 // Root React component.
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 
-// Define our inital state object. This could be a fetch() to an API endpoint.
-const initialState = window.__data || {}
-// Configure and create our Redux store.
-const store = configureStore(initialState)
-
 // Define our destination where we insert our root react component.
 const destEl = document.getElementById('root')
 
-// The root component needs the Redux `store` and router history as props.
-render(<Root store={store} history={history} />, destEl)
+// Define our inital state object. This could be a fetch() to an API endpoint.
+fetch('/data.json', {
+  headers: {
+    Accept: 'application/json',
+  },
+  method: 'get',
+})
+.then(response =>
+  response.json()
+)
+.then(json => {
+  const initialState = { db: json }
+  // Configure and create our Redux store.
+  const store = configureStore(initialState)
+  // The root component needs the Redux `store` and router history as props.
+  render(<Root store={store} history={history} />, destEl)
+})
