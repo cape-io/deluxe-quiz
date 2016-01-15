6065a8b344a3f453efcac6851c31a40082096802
Kai Curry <kai@cape.io>
2016-01-14 23:36:52 -0600

    input radios

---
+++ src/components/Form/InputRadios.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+import RadioOption from './RadioOption'
+
+// Print RadioOption for each options.
+function InputRadios({ options, field, ...rest }) {
+  return (
+    <div>
+      {
+        options.map(({ value, label }) => {
+          const id = `{$field.name}-${value}`
+          return (
+            <RadioOption
+              id={id}
+              key={id}
+              field={field}
+              value={value}
+              label={label}
+              {...rest}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+InputRadios.propTypes = {
+  options: PropTypes.array.isRequired,
+}
+
+export default InputRadios
