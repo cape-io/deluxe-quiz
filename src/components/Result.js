5bd67a4b5be32dc6cb7bed4221b331318536d633
Kai Curry <kai@cape.io>
2016-01-15 11:24:30 -0600

    print out more

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,26 @@
 import React, { PropTypes } from 'react'
-
+import map from 'lodash/map'
 function Result({ lead, header, points, score, resultBox }) {
+  const { scoreTxt, submit } = resultBox
   return (
     <div>
-      <div className="top">
-        <div></div>
+      <div className="score-top">
+        <div className="score-text">{scoreTxt}</div>
+        <div>
+          <div className="score-box">{score}</div>
+          <div>
+            <p><strong>{lead[0]}</strong> {lead[1]}</p>
+            <p>{resultBox.lead[0]} <strong>{resultBox.lead[1]}</strong></p>
+          </div>
+        </div>
+      </div>
+      <div className="score-bottom">
+        <h2>{header}</h2>
+        <ul>
+          {
+            map(points, (point, index) => <li key={index}>{point}</li>)
+          }
+        </ul>
       </div>
     </div>
   )
