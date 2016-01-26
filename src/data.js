1ee71c0be4f5dfd2641a3d3e11ba954247119895
Kai Curry <kai@cape.io>
2016-01-26 09:23:56 -0600

    compile json into bundle

---
+++ src/data.js
@@ -0,0 +1,123 @@
+export default {
+  "author": "Deluxe Marketing Services for Customer Insights",
+  "title": "BEST-PRACTICES QUIZ",
+  "logo": {
+    "src": "/images/logo.jpg",
+    "alt": "Deluxe Financial Services",
+    "href": "http://fi.deluxe.com/"
+  },
+  "intro": "Do you truly understand your account holders’ needs and perceptions? Are you gathering the key drivers that matter to your account holders, allowing you to take action?\n",
+  "lead": "TAKE THIS QUICK QUIZ TO FIND OUT.",
+  "questions": [
+    {
+      "id": "account-holders",
+      "text": "Are you surveying account holders about their experience in multiple channels: in branch, on the phone, in the drive-through, online and via mobile access?\n"
+    },
+    {
+      "id": "loan",
+      "text": "Are you including questions about loan services, small-business services and all other key offerings?"
+    },
+    {
+      "id": "promote",
+      "text": "Are you gauging how likely account holders are to promote your financial institution?"
+    },
+    {
+      "id": "feedback-evaluate",
+      "text": "Are you evaluating account holder feedback weekly or even daily?"
+    },
+    {
+      "id": "feedback-impact",
+      "text": "Do you implement feedback so it makes a noticeable impact?"
+    },
+    {
+      "id": "feedback-importance",
+      "text": "Do employees understand why implementing feedback is important?"
+    },
+    {
+      "id": "benchmark",
+      "text": "Are you able to benchmark your results against the competition?"
+    },
+    {
+      "id": "improvement",
+      "text": "Do you know your current top three opportunities for improvement?"
+    }
+  ],
+  "options": [
+    {
+      "label": "Always",
+      "value": 2
+    },
+    {
+      "label": "Sometimes",
+      "value": 1
+    },
+    {
+      "label": "Never",
+      "value": 0
+    }
+  ],
+  "submit": "Submit Answers",
+  "scores": [
+    {
+      "id": "best",
+      "min": 10,
+      "lead": [
+        "Looks like you’ve made some smart choices.",
+        "Let’s make your efforts even more actionable."
+      ],
+      "header": "Our customer insights solution goes beyond traditional customer satisfaction efforts to deliver results that are truly meaningful to financial institutions including",
+      "points": [
+        "Ongoing feedback from a statistically significant panel of your actual account holders.",
+        "Real-time, actionable reports that benchmark your performance against competitors and show trends within your institution.",
+        "Clear insights on how you can improve processes and coach employees toward achieving a higher level of account holder satisfaction and loyalty."
+      ],
+      "color": "9BB73B"
+    },
+    {
+      "id": "medium",
+      "min": 6,
+      "lead": [
+        "Nice start.",
+        "Now let’s figure out how you can get a 360° view of what’s really important."
+      ],
+      "header": "Our customer insights solution can help you take your account holder satisfaction efforts to the next level by",
+      "points": [
+        "Benchmarking your performance against other financial institutions.",
+        "Providing real-time access to data that enables timely coaching and recognition opportunities for your employees.",
+        "Turning metrics into actionable advice that increases revenue."
+      ],
+      "color": "EEBD1C"
+    },
+    {
+      "id": "bad",
+      "min": 0,
+      "lead": [
+        "It’s time to get started."
+      ],
+      "header": "With an approach tailored to financial institutions, our customer insights solution can help you implement a customer satisfaction program that",
+      "points": [
+        "Collects regular feedback and uncovers what your account holders are really thinking about your employees, facilities, products and services.",
+        "Uncovers key opportunities to improve your processes and better meet account holders’ wants and needs.",
+        "Makes program administration easy and requires no additional staff resources."
+      ],
+      "color": "DA291C"
+    }
+  ],
+  "resultBox": {
+    "scoreTxt": "Your score",
+    "lead": "Check out"
+  },
+  "contact": {
+    "email": "deluxefinancialservices@deluxe.com",
+    "phone": "(877) 214-2513",
+    "web": "fi.deluxe.com",
+    "name": "Deluxe Corporate Headquarters",
+    "address": "3680 Victoria Street North, Shoreview, MN 55126-2966"
+  },
+  "copyright": "Deluxe Financial Services, LLC",
+  "learnMore": {
+    "label": "See how it works.",
+    "link": "http://insight2action.deluxe.com/MS-2016-01-CI-LP1.html",
+    "button": "Learn more"
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,30 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 // Add the latest es2016+ stuff that we love.
 import 'babel-polyfill'
-import 'isomorphic-fetch'
 
 // Root React component.
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
-
+import json from './data'
 // Define our destination where we insert our root react component.
 const destEl = document.getElementById('root')
 
 // Define our inital state object. This could be a fetch() to an API endpoint.
-fetch('/data.json?v=10', {
-  headers: {
-    Accept: 'application/json',
-  },
-  method: 'get',
-})
-.then(response =>
-  response.json()
-)
-.then(json => {
-  json.currentYear = new Date().getFullYear()
-  const initialState = { db: json }
-  // Configure and create our Redux store.
-  const store = configureStore(initialState)
-  // The root component needs the Redux `store` and router history as props.
-  render(<Root store={store} history={history} />, destEl)
-})
+json.currentYear = new Date().getFullYear()
+const initialState = { db: json }
+// Configure and create our Redux store.
+const store = configureStore(initialState)
+// The root component needs the Redux `store` and router history as props.
+render(<Root store={store} history={history} />, destEl)
